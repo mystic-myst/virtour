28f6bcedadf51f983b45c271f1fa2a2e260232b9
mystic-myst <74984249+mystic-myst@users.noreply.github.com>
2023-07-18 12:18:31 +0530

    New Tour 2

--- data.js
+++ data.js
@@ -1,255 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-the-armoury",
-      "name": "The Armoury",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-the-billiard-room",
-      "name": "The Billiard Room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-the-dining-room",
-      "name": "The Dining Room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-the-great-drawing-room",
-      "name": "The Great Drawing Room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-the-ladies-drawing-room",
-      "name": "The Ladies' Drawing Room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-the-morning-room",
-      "name": "The Morning Room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-the-porcelain-room",
-      "name": "The Porcelain Room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "7-the-smoking-room",
-      "name": "The Smoking Room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "8-the-upper-vestibule",
+      "id": "0-the-upper-vestibule",
       "name": "The Upper Vestibule",
       "levels": [
         {
@@ -272,11 +24,388 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.13955763897980233,
+        "pitch": -0.12460652740401557,
+        "fov": 1.2498696229631854
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.14476335808339869,
+          "pitch": -0.0827943750191178,
+          "rotation": 0,
+          "target": "6-the-morning-room"
+        },
+        {
+          "yaw": 1.3105989255040047,
+          "pitch": 0.044758203007464914,
+          "rotation": 0,
+          "target": "5-the-ladies-drawing-room"
+        },
+        {
+          "yaw": -1.3042973684427963,
+          "pitch": -0.13173010707964927,
+          "rotation": 0,
+          "target": "3-the-dining-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-the-armoury",
+      "name": "The Armoury",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.49955398752048,
+        "pitch": 0.0165493198674298,
+        "fov": 1.081981317260399
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.37594603669437276,
+          "pitch": 0.10374387315866329,
+          "rotation": 0,
+          "target": "8-the-smoking-room"
+        },
+        {
+          "yaw": -2.949745780795082,
+          "pitch": 0.12079239889457938,
+          "rotation": 0,
+          "target": "2-the-billiard-room"
+        },
+        {
+          "yaw": 1.434085103547286,
+          "pitch": 0.07126035714339096,
+          "rotation": 0,
+          "target": "6-the-morning-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-the-billiard-room",
+      "name": "The Billiard Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3274289077235746
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20895823814328374,
+          "pitch": -0.0054589061283429885,
+          "rotation": 0,
+          "target": "7-the-porcelain-room"
+        },
+        {
+          "yaw": 3.0980504396654824,
+          "pitch": 0.046633719305347654,
+          "rotation": 0,
+          "target": "1-the-armoury"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-the-dining-room",
+      "name": "The Dining Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.11276657716484628,
+        "pitch": -0.1319837325115536,
+        "fov": 0.9174496944899682
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8171216945169872,
+          "pitch": 0.04906754726014029,
+          "rotation": 0,
+          "target": "0-the-upper-vestibule"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-the-great-drawing-room",
+      "name": "The Great Drawing Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.2343882353082396,
+        "pitch": -0.21524643163442647,
+        "fov": 0.9174496944899682
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3958425619315982,
+          "pitch": -0.19984385458073461,
+          "rotation": 0,
+          "target": "8-the-smoking-room"
+        },
+        {
+          "yaw": 3.13670094611485,
+          "pitch": -0.16095141713253902,
+          "rotation": 0,
+          "target": "6-the-morning-room"
+        },
+        {
+          "yaw": 1.4132595338298923,
+          "pitch": -0.18488939864293918,
+          "rotation": 0,
+          "target": "5-the-ladies-drawing-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-the-ladies-drawing-room",
+      "name": "The Ladies' Drawing Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.13635446232084547,
+        "pitch": 0.08511548824535176,
+        "fov": 1.3274289077235746
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5628894314932715,
+          "pitch": -0.05700951912954366,
+          "rotation": 4.71238898038469,
+          "target": "4-the-great-drawing-room"
+        },
+        {
+          "yaw": 1.5394581025877025,
+          "pitch": 0.2862309574636228,
+          "rotation": 0,
+          "target": "0-the-upper-vestibule"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-the-morning-room",
+      "name": "The Morning Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.04794022529969588,
+        "pitch": -0.029987098129947043,
+        "fov": 1.3274289077235746
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.021924356606664475,
+          "pitch": -0.08906759005642151,
+          "rotation": 0,
+          "target": "4-the-great-drawing-room"
+        },
+        {
+          "yaw": -1.1052210012555364,
+          "pitch": -0.12191946936453135,
+          "rotation": 0,
+          "target": "1-the-armoury"
+        },
+        {
+          "yaw": 1.0373086160235907,
+          "pitch": -0.05974784498374497,
+          "rotation": 0,
+          "target": "0-the-upper-vestibule"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-the-porcelain-room",
+      "name": "The Porcelain Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.4933917750834773,
+        "pitch": -0.08152354502291281,
+        "fov": 1.0061583293209468
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9309875512724304,
+          "pitch": -0.0320171610399953,
+          "rotation": 0,
+          "target": "2-the-billiard-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-the-smoking-room",
+      "name": "The Smoking Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.1683365086931161,
+        "pitch": 0.05997419625989231,
+        "fov": 1.3274289077235746
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03533326834923933,
+          "pitch": 0.026488234469614014,
+          "rotation": 0,
+          "target": "1-the-armoury"
+        },
+        {
+          "yaw": -1.5224807500021047,
+          "pitch": 0.11397249903236073,
+          "rotation": 0,
+          "target": "4-the-great-drawing-room"
+        }
+      ],
       "infoHotspots": []
     }
   ],
@@ -284,7 +413,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
